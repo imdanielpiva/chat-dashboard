4b6919d494582ceb0c6e5c42ecd1b65f144e7f59
daniel piva <imdanielpiva@gmail.com>
2017-10-16 14:48:25 -0200

    added DashChat component

---
+++ src/components/DashChat/DashChat.vue
@@ -0,0 +1,65 @@
+<template>
+  <div
+    class="q-message"
+    :class="{
+      'q-message-sent': side,
+      'q-message-received': !side
+    }"
+  >
+    <p v-if="label" class="q-message-label text-center">{{ label }}</p>
+    <div v-if="avatar" class="q-message-container row items-end no-wrap">
+      <slot name="avatar">
+        <img class="q-message-avatar" :src="avatar">
+      </slot>
+      <div class="column">
+        <div v-if="name" class="q-message-name">{{ name }}</div>
+        <div
+          v-for="(msg, index) in text"
+          :key="index"
+          class="q-message-text"
+          :class="messageClass"
+        >
+          <span class="q-message-text-content" :class="textClass">
+            <p>{{ msg }}</p>
+            <!-- <chat-stamp :props="{ status, id }" /> -->
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dash-chat',
+  props: {
+    id: String,
+    side: Boolean,
+    label: String,
+    bgColor: String,
+    textColor: String,
+    name: String,
+    avatar: String,
+    text: Array,
+    status: Object,
+    spinner: Object
+  },
+  computed: {
+    textClass () {
+      if (this.textColor) {
+        return `text-${this.textColor}`
+      }
+    },
+    messageClass () {
+      if (this.bgColor) {
+        return `text-${this.bgColor}`
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
