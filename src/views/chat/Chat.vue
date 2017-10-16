5115e8b41be60b0f333c52b3cfe9bbd7bf901f52
daniel piva <imdanielpiva@gmail.com>
2017-10-16 16:57:00 -0200

    added qtoolbar structure

---
+++ src/views/chat/Chat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <dash-chat></dash-chat>
+  </div>
+</template>
+
+<script>
+import DashChat from '../../components/DashChat/DashChat';
+
+export default {
+  name: 'chat',
+  components: {
+    DashChat
+  }
+};
+</script>
+
+<style>
+
+</style>
